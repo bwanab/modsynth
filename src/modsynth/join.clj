9a8899fe9264df9144847a18e44ac1f2d00bd016
Bill Allen <bill@k2bea.org>
2014-04-12 11:11:07 -0400

    get basic version of join-on-common working

--- src/modsynth/join.clj
+++ src/modsynth/join.clj
@@ -1,4 +1,4 @@
-(ns cljivtk.join)
+(ns modsynth.join)
 
 (defn find-common
   [s1 s2]
@@ -14,9 +14,18 @@
   [s1 s2]
   (let [common (find-common s1 s2)]
     (loop [cc common t1 s1 t2 s2 acc []]
-      (if (empty? cc) (concat acc (rest t1) (rest t2))
-          (let [c (first cc)
-                p1 (split-with #(not= c %) t1)
-                p2 (split-with #(not= c %) t2)
-                [l1 l2] (longer-first (first p1) (first p2))]
-            (recur (rest cc) (second p1) (second p2) (concat l1 l2 [c])))))))
+      (do
+        (println t1 t2 acc)
+        (if (empty? cc) (concat acc t1 t2)
+           (let [c (first cc)
+                 p1 (split-with #(not= c %) t1)
+                 p2 (split-with #(not= c %) t2)
+                 [l1 l2] (longer-first (first p1) (first p2))]
+             (recur (rest cc) (rest (second p1)) (rest (second p2)) (concat acc l1 l2 [c]))))))))
+
+(defn test []
+  (let [s1 [:a :b :c :d :e]
+        s2 [:f :b :g :d :h]
+        res (join-on-common s1 s2)]
+    (println res)
+    (= res [:a :f :b :c :g :d :e :h])))
