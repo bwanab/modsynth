e6e59c8546222ffd559583aaf9b9c5d61591ab01
William Allen <williamallen@Williams-iMac.local>
2020-10-29 15:48:07 -0400

    reverting clojure and overtone to previous versions since midi isn't working

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main modsynth.core
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [seesaw "1.5.0"]
-                 [overtone "0.10.6"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [seesaw "1.4.4"]
+                 [overtone "0.9.1"]
                  [leipzig "0.10.0"]])
