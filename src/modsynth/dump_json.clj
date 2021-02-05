5f485ab57d739cac6bf5da2d10415cd28d20971a
Bill Allen <bill@k2bea.org>
2021-02-05 11:49:16 -0500

    added ability to dump oms files as json - finished

--- src/modsynth/dump_json.clj
+++ src/modsynth/dump_json.clj
@@ -1,8 +1,37 @@
 (ns modsynth.dump-json
-  (:require [clj-json [core :as json]]))
+  (:require [clj-json [core :as json]]
+            [clojure.java.io :as io]
+            [clojure.string :as string]))
+
 
 (defn dump-file [inf outf]
-  (let [data (load-file inf)
-        js (json/generate-string data)
-        mjs (clojure.string/replace (clojure.string/replace js ",{" ",\n{") ",[" ",\n[")]
+  (let [mjs
+        (-> inf
+            (load-file)
+            (json/generate-string)
+            (string/replace ":[" ":\n[")
+            (string/replace ":{" ":\n{")
+            (string/replace ",{" ",\n{")
+            (string/replace "]," "],\n")
+            )]
     (spit outf mjs)))
+
+(defn dump-to [fname ind outd]
+  (let [inf (string/join "/" [ind fname])
+        outf (string/join "/" [outd (string/replace fname ".oms" ".json")])]
+    (println inf outf)
+    [inf outf]))
+
+(defn mymap [coll f]
+  (map f coll))
+
+(defn myapply [coll f]
+  (apply f coll))
+
+(defn dump-all-files [ind outd]
+  (-> ind
+      (io/file)
+      (.list)
+      (mymap #(-> %
+                  (dump-to ind outd)
+                  (myapply dump-file)))))
